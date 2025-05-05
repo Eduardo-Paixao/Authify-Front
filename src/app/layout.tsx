1844d6c32321f42a625bac9adb84385df6b1d62b
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2025-05-05 01:19:38 -0300

    chore: remove ToggleThemeButton and ImgeBackground in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,6 @@ import client from "@/lib/apollo";
 import { ToastContextProvider } from "@/contexts/ToastfyContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import ToggleThemeButton from "@/components/ToggleThemeButton";
-import ImgeBackground from "@/components/ImgeBackground";
-import { useTheme } from "@/hooks/useTheme";
 
 const KdamThmorPro = Kdam_Thmor_Pro({
   weight:"400",
@@ -20,14 +17,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { theme, toggleTheme } = useTheme();
+
   return (
     <html lang="pt-BR" className={KdamThmorPro.className}>
       <body>
         <ApolloProvider client={client}>
           <ToastContextProvider>
-            <ToggleThemeButton toggleTheme={toggleTheme} />
-            <ImgeBackground theme={theme === "dark"} />
+            
             {children}
             <ToastContainer theme="dark" />
           </ToastContextProvider>
